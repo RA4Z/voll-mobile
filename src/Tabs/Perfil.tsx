36bcae8f9daac8c769210e587ed5507261756473
RA4Z <rraz639@gmail.com>
2023-06-04 18:30:49 -0300

    Estilização da tela de perfil

--- src/Tabs/Perfil.tsx
+++ src/Tabs/Perfil.tsx
@@ -1,9 +1,22 @@
-import { VStack, Text } from 'native-base';
+import { VStack, Text, ScrollView, Avatar, Divider } from 'native-base';
+import { Titulo } from '../components/Titulo';
 
 export default function Perfil() {
     return (
-        <VStack>
-            <Text>Perfil</Text>
-        </VStack>
+        <ScrollView flex={1}>
+            <VStack flex={1} alignItems='center' p={5}>
+                <Titulo color='blue.500'>Meu Perfil</Titulo>
+                <Avatar size='xl' source={{ uri: 'https://github.com/RA4Z.png' }} mt={5} />
+                
+                <Titulo color='blue.500'>Informações pessoais</Titulo>
+                <Titulo fontSize='lg' mb={1} >Robert Aron Zimmermann</Titulo>
+                <Text>31/07/2003</Text>
+                <Text>Schroeder - SC</Text>
+                <Divider mt={5} />
+                <Titulo color='blue.500' mb={1}>Histórico médico</Titulo>
+                <Text>Bronquite</Text>
+                <Text>Sinusite</Text>
+            </VStack>
+        </ScrollView>
     )
 }
