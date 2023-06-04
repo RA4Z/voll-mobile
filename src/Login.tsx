fa16f93338fa927551fb450ca026821d9d42491d
RA4Z <rraz639@gmail.com>
2023-06-04 11:25:04 -0300

    Finalizando tela de Login

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,9 +1,10 @@
-import { VStack, Image, Text, Box, FormControl, Input } from 'native-base';
+import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base';
+import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png'
 
 export default function Login() {
   return (
-    <VStack flex={1} alignItems="center" p={5}>
+    <VStack flex={1} alignItems="center" justifyContent='center' p={5}>
        <Image source={Logo} alt="Logo Voll" />
 
        <Text
@@ -40,6 +41,27 @@ export default function Login() {
             />
           </FormControl>
        </Box>
+
+      <Button 
+          w='100%'
+          bg='blue.800'
+          mt={10}
+          borderRadius='lg'
+          >
+        Entrar
+      </Button>
+
+      <Link href='https://www.alura.com.br'>
+        Esqueceu sua senha?
+      </Link>
+      <Box w='100%' flexDirection='row' justifyContent='center'>
+        <Text>Ainda não tem cadastro? </Text>
+        <TouchableOpacity>
+          <Text color='blue.500'> 
+            Faça seu cadastro!
+          </Text>
+        </TouchableOpacity>
+      </Box>
     </VStack>
   );
 }
