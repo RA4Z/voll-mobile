b3a8fe220a1b88f0b0f487c7a1fb026a14f59986
RA4Z <rraz639@gmail.com>
2023-06-04 11:33:27 -0300

    Fazendo o Título ser um componente

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,22 +1,15 @@
 import { VStack, Image, Text, Box, FormControl, Input, Button, Link } from 'native-base';
 import { TouchableOpacity } from 'react-native';
 import Logo from './assets/Logo.png'
+import { Titulo } from './components/Titulo';
 
 export default function Login() {
   return (
     <VStack flex={1} alignItems="center" justifyContent='center' p={5}>
-       <Image source={Logo} alt="Logo Voll" />
+        <Image source={Logo} alt="Logo Voll" />
 
-       <Text
-        fontSize="2xl"
-        fontWeight="bold"
-        color="gray.500"
-        textAlign="center"
-        marginTop={5}
-       >
-        Faça Login em sua conta
-       </Text>
-       <Box>
+        <Titulo>Faça Login em sua conta</Titulo>
+        <Box>
           <FormControl mt={3}>
             <FormControl.Label>Email</FormControl.Label>
             <Input 
@@ -40,7 +33,7 @@ export default function Login() {
               shadow={3}
             />
           </FormControl>
-       </Box>
+        </Box>
 
       <Button 
           w='100%'
@@ -51,10 +44,11 @@ export default function Login() {
         Entrar
       </Button>
 
-      <Link href='https://www.alura.com.br'>
+      <Link href='https://www.alura.com.br' mt={2}>
         Esqueceu sua senha?
       </Link>
-      <Box w='100%' flexDirection='row' justifyContent='center'>
+
+      <Box w='100%' flexDirection='row' justifyContent='center' mt={8}>
         <Text>Ainda não tem cadastro? </Text>
         <TouchableOpacity>
           <Text color='blue.500'> 
