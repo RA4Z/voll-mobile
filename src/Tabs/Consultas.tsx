fef97fc609ccf4d7d2170cd4cacf0496f005e321
RA4Z <rraz639@gmail.com>
2023-06-05 18:07:13 -0300

    Criação do card das consultas

--- src/Tabs/Consultas.tsx
+++ src/Tabs/Consultas.tsx
@@ -1,9 +1,12 @@
 import { VStack, Text } from 'native-base';
+import { CardConsulta } from '../components/CardConsulta';
 
 export default function Consultas() {
     return (
-        <VStack>
+        <VStack p={5}>
             <Text>Consultas</Text>
+            <CardConsulta nome='Dr. Robert'  data='20/04/2023'
+            especialidade='Cardiologista' foto='https://github.com/RA4Z.png'/>
         </VStack>
     )
 }
